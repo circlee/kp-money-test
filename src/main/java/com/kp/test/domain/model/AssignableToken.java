33dbd63eef5fa9fb1c962cd27a31d942cb202ef1
circlee <circlee7@gmail.com>
2020-06-25 18:40:08 +0900

    feat : AssignableToken 재할당 처리

--- src/main/java/com/kp/test/domain/model/AssignableToken.java
+++ src/main/java/com/kp/test/domain/model/AssignableToken.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.persistence.Version;
 
 import com.kp.test.domain.vo.Token;
 import com.kp.test.domain.vo.UserId;
@@ -13,15 +14,19 @@ import com.kp.test.domain.vo.UserId;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
+@Setter(value = AccessLevel.PROTECTED)
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Entity
+@EqualsAndHashCode
 public class AssignableToken implements Serializable {
 	
 	private static final long serialVersionUID = 7884065933262181778L;
@@ -38,6 +43,9 @@ public class AssignableToken implements Serializable {
 	
 	private LocalDateTime assignedAt;
 	
+	@Version
+	private Long version;
+	
 	public static AssignableToken assignCreate(Token token, UserId userId) {
 		
 		LocalDateTime assignedAt = LocalDateTime.now();
@@ -49,5 +57,18 @@ public class AssignableToken implements Serializable {
 				.assignedBy(userId)
 				.assignedAt(assignedAt)
 				.build();
+	}
+	
+	public void assign(UserId userId) {
+		if(!LocalDateTime.now().isAfter(this.getExpiredAt())) {
+			throw new IllegalStateException("expiredAt 이전에 할당 할 수 없습니다.");
+		}
+		
+		LocalDateTime assignedAt = LocalDateTime.now();
+		LocalDateTime expiredAt = assignedAt.plusDays(TOKEN_ACTIVE_DAY);
+		
+		this.setExpiredAt(expiredAt);
+		this.setAssignedBy(userId);
+		this.setAssignedAt(assignedAt);
 	}
 }
--- src/test/java/com/kp/test/domain/service/AssignableTokenCreatorTest.java
+++ src/test/java/com/kp/test/domain/service/AssignableTokenCreatorTest.java
@@ -5,16 +5,24 @@
 */
 package com.kp.test.domain.service;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.kp.test.KpMoneySpreadApplication;
 import com.kp.test.domain.model.AssignableToken;
 import com.kp.test.domain.repository.AssignableTokenRepository;
+import com.kp.test.domain.vo.Token;
 import com.kp.test.domain.vo.UserId;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,8 +33,9 @@ import lombok.extern.slf4j.Slf4j;
 * AssignableTokenCreatorTest.java
 */
 @Slf4j
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+@ExtendWith(value = {SpringExtension.class})
 @SpringBootTest(classes = KpMoneySpreadApplication.class)
-@TestPropertySource(properties = { "spring.config.location=classpath:application.yml" })
 public class AssignableTokenCreatorTest {
 
 	@Autowired
@@ -35,7 +44,7 @@ public class AssignableTokenCreatorTest {
 	@Autowired
 	private TokenGenerator tokenGenerator;
 	
-	
+	@Order(0)
 	@DisplayName("유저 할당 토큰 생성 테스트")
 	@Test
 	public void assignCreatTest() {
@@ -50,5 +59,49 @@ public class AssignableTokenCreatorTest {
 		
 		Assertions.assertNotNull(assignableToken);
 		Assertions.assertEquals(assignableToken.getAssignedBy(), testUser100);
+		
+		Optional<AssignableToken> savedAssignableToken = assignableTokenRepository.findById(assignableToken.getToken());
+		
+		Assertions.assertTrue(savedAssignableToken.isPresent());
+		Assertions.assertEquals(assignableToken, savedAssignableToken.get());
+		
+	
+	}
+	
+	@Order(1)
+	@DisplayName("중복 유저 할당 토큰 생성 예외 테스트")
+	@Test
+	public void duplicateTokenCreateTest() {
+		
+		UserId testUser100 = UserId.from(100L);
+		
+		TokenGenerator fixedTokenGenerator = () -> Token.from("FIX");
+		
+		AssignableTokenCreator tokenCreator = new AssignableTokenCreator(assignableTokenRepository, fixedTokenGenerator);
+		
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+			tokenCreator.createToken(testUser100);
+			tokenCreator.createToken(testUser100);
+		});
+	}
+	
+	@Order(2)
+	@DisplayName("expiredAt 이전 토큰 재할당 예외 테스트")
+	@Test
+	public void reAssignErrorTest() {
+		
+		UserId testUser100 = UserId.from(100L);
+		UserId testUser200 = UserId.from(200L);
+		
+		TokenGenerator fixedTokenGenerator = () -> Token.from("TE1");
+		
+		AssignableTokenCreator tokenCreator = new AssignableTokenCreator(assignableTokenRepository, fixedTokenGenerator);
+		
+		AssignableToken assignableToken1 = tokenCreator.createToken(testUser100);
+		
+		Assertions.assertThrows(IllegalStateException.class, () -> {
+			assignableToken1.assign(testUser200);	
+		});
+
 	}
 }
