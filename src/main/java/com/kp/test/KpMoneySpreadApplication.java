d3f980712e2c0a4d1bc5ab92884450867331064e
eldie <eldie@whitin.co.kr>
2020-06-25 16:45:15 +0900

    init : project 생성

---
+++ src/main/java/com/kp/test/KpMoneySpreadApplication.java
@@ -0,0 +1,14 @@
+package com.kp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KpMoneySpreadApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KpMoneySpreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kp/test/KpMoneySpreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpMoneySpreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
