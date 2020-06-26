71a7fb19d7df5e1dcf5d66f6827576ab9dacc589
circlee <circlee7@gmail.com>
2020-06-26 19:07:18 +0900

    feat : MoneySpread 조회 Pessimistic Lock 설정

--- src/main/java/com/kp/test/domain/repository/MoneySpreadRepository.java
+++ src/main/java/com/kp/test/domain/repository/MoneySpreadRepository.java
@@ -4,13 +4,16 @@ import com.kp.test.domain.model.MoneySpread;
 import com.kp.test.domain.vo.MoneySpreadTokenId;
 import com.kp.test.domain.vo.MoneySpreadId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface MoneySpreadRepository extends JpaRepository<MoneySpread, MoneySpreadId> {
 
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     Optional<MoneySpread> findByMoneySpreadTokenId(MoneySpreadTokenId moneySpreadTokenId);
 
 }
